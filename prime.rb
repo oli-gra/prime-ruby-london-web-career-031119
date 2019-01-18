b5d275b66295e8343f621aac38fbfa1f41e545e9
Oliver <oliver.grant@gmail.com>
2019-01-18 20:14:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+def prime?(n)
+  # cannot use Prime so... 
+  # 1st test n==1 or n%2==0 not prime
+  if n<2 || n%2==0
+    return prime?=false
+  end
+  # 2nd test Sieve of Erathosthenes
+  # load all numbers to n into an array
+  erathos=[2..n]
+  i=0
+  # loop until we reach then end of the array
+  until i==erathos.length
+    if n==erathos[i]
+      return prime?=true
+    else
+      # p=prime_number, delete elements 2p, 3p.. from array
+      j=2
+      while j*erathos[i]<n
+        erathos-=[j*erathos[i]]
+        j+=1
+      end
+      # next_prime_number
+      i+=1
+    end
+  end
+  return prime?=false
+end
